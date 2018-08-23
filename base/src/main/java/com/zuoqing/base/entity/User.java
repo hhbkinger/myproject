281853667e325d45e3ba3db3e88296a1746bb441
hhbkinger <hhbkinger@163.com>
2018-08-23 16:08:05 +0800

    1

--- base/src/main/java/com/zuoqing/base/entity/User.java
+++ base/src/main/java/com/zuoqing/base/entity/User.java
@@ -8,9 +8,11 @@ import java.io.Serializable;
 public class User implements Serializable{   
     private String name;
     private int age;
+    
 
     public int getAge() {
         return age;
+        
     }
 
     public void setAge(int age) {
